d6bf912bd4d5e5722a437697e49f8b2b8e2c4e5a
DhruviShah16 <19dce123@charusat.edu.in>
2025-05-06 23:52:20 +0530

    convert file from jsx to tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TodoWrapper from "./components/TodoWrapper";
+// import Callback from "./components/Callback";
+import "./App.css";
+function App() {
+  return (
+    <div className="container flex  align-items-center justify-center flex-col   ">
+      <TodoWrapper />
+      {/* <Callback /> */}
+    </div>
+  );
+}
+export default App;
